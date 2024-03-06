486ddf936a7d143cc2e40ecd2c4056d00145c4f5
aryanzero <chauhanaryan2000@gmail.com>
2024-03-06 12:11:22 -0500

    tried bidding model creation

--- sneaksbid/models.py
+++ sneaksbid/models.py
@@ -1,23 +1,38 @@
 from django.db import models
 from django.conf import settings
+from datetime import timedelta
+from django.utils import timezone
+
 
 # Create your models here.
 class Item(models.Model):
-    title = models.CharField(max_length=100)
-    price = models.FloatField()
-    stock_no = models.CharField(max_length=10)
-    description_short = models.CharField(max_length=50)
-    description_long = models.TextField()
-    image = models.ImageField()
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+    current_bid = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    bid_expiry = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
-    available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
-    
+
+    def update_expiry_time(self):
+        self.bid_expiry = timezone.now() + timedelta(minutes=10)  # Reset timer to 10 minutes from now
+        self.save()
+
+
+class Bid(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='bids')
+    bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    bid_time = models.DateTimeField(auto_now_add=True)
+
+    # def __str__(self):
+    #     return f"{self.user.username} - {self.bid_amount}"
+
+
 class OrderItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                            on_delete=models.CASCADE)
+                             on_delete=models.CASCADE)
     ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
@@ -26,15 +41,4 @@ class OrderItem(models.Model):
         return f"{self.quantity} of {self.item.title}"
 
     def get_total_item_price(self):
-        return self.quantity * self.item.price
-
-    def get_total_discount_item_price(self):
-        return self.quantity * self.item.discount_price
-
-    def get_amount_saved(self):
-        return self.get_total_item_price() - self.get_total_discount_item_price()
-
-    def get_final_price(self):
-        if self.item.discount_price:
-            return self.get_total_discount_item_price()
-        return self.get_total_item_price()
+        return self.quantity * self.item.current_bid
