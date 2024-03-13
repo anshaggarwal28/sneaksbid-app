b61cee041eee997cf70aefe3ac11e7436bcb2adb
Sahibjeet Singh <sahibsingh318.ss@gmail.com>
2024-03-12 20:29:58 -0400

    connecting shop to bid view

--- snekasbiddjangoProject/urls.py
+++ snekasbiddjangoProject/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('signout', views.signout, name='signout'),
     path('signup', views.signup, name='signup'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
-    path('shop', views.shop, name='shop'),
+    path('shop/', views.shop, name='shop'),
     path('item/<int:item_id>/', views.item_detail, name='item_detail'),
     path('item/<int:item_id>/bid/', views.place_bid, name='place_bid'),
 ]
