7ef35f3b1228bce13462af08c768928d1e688364
Maahi5656 <77339007+Maahi5656@users.noreply.github.com>
2024-03-19 21:41:10 -0400

    Updated 'urls.py'. Added the url for the reset password

--- snekasbiddjangoProject/urls.py
+++ snekasbiddjangoProject/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from sneaksbid import views
-from sneaksbid.views import HomeView, shop
+from sneaksbid.views import HomeView, shop, ShoeCreateView,CheckoutView,Payment
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +33,21 @@ urlpatterns = [
     path('shop/', views.shop, name='shop'),
     path('item/<int:item_id>/', views.item_detail, name='item_detail'),
     path('item/<int:item_id>/bid/', views.place_bid, name='place_bid'),
+    #path('payment/', views.payment, name='payment'),
+    path('process_payment/<str:client_secret>/', views.process_payment, name='process_payment'),
+    path('add-shoe/', ShoeCreateView.as_view(), name='add_shoe'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('search/', views.search_sneakers, name='search_sneakers'),
+
+   # path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+   # path('checkout/success/', checkout_success_view, name='checkout_success'),
+
+    # Forgot Password
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='authentication/password_reset.html'), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='authentication/password_reset_sent.html'), name="password_reset_done"),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='authentication/password_reset_form.html'), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='authentication/password_reset_done.html'), name="password_reset_complete")
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
