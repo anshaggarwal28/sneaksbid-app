e72fa493b2272d23824283e04c09829fdfb1e771
aryanzero <chauhanaryan2000@gmail.com>
2024-03-22 09:37:56 -0400

    added req.txt

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
-python3.9 -m pip install -r Django
-python3.9 -m pip install -r dj-stripe
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
