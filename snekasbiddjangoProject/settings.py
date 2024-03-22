c101995bb30ad368400ee1deec8dc4d5add6712b
aryanzero <chauhanaryan2000@gmail.com>
2024-03-22 13:12:13 -0400

    settings working

--- snekasbiddjangoProject/settings.py
+++ snekasbiddjangoProject/settings.py
@@ -101,24 +101,24 @@ WSGI_APPLICATION = 'snekasbiddjangoProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'bvofwmEjWiNzlAzBovGYzUvcizfAYgdG',
-        'HOST': 'viaduct.proxy.rlwy.net',
-        'PORT': '51602',
-    }
-}
-
+#
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'bvofwmEjWiNzlAzBovGYzUvcizfAYgdG',
+#         'HOST': 'viaduct.proxy.rlwy.net',
+#         'PORT': '51602',
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
