8db6603c032585da2420f88af7826920281c403b
aryanzero <chauhanaryan2000@gmail.com>
2024-03-22 15:48:40 -0400

    updated

--- snekasbiddjangoProject/settings.py
+++ snekasbiddjangoProject/settings.py
@@ -162,10 +162,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-<<<<<<< HEAD
 
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-=======
->>>>>>> parent of 315ba39 (vercel changes added everything works fine)
