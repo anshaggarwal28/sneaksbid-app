04a876ff9a8cd0f6215cdc11fc8560bc4ec3dfdf
aryanzero <chauhanaryan2000@gmail.com>
2024-03-25 19:00:09 -0400

    dfew

--- snekasbiddjangoProject/settings.py
+++ snekasbiddjangoProject/settings.py
@@ -159,6 +159,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
